9e6fcbdbac18582856b0a54bebeb78b293fe7891
wang <374951963@qq.com>
2017-04-04 14:57:30 +0800

    Main2Activity用贝塞尔曲线实现实现波浪效果

--- app/src/main/java/com/demo/waveuidemo/MainActivity.java
+++ app/src/main/java/com/demo/waveuidemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.demo.waveuidemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.go_to_main2_bt);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+            }
+        });
     }
 }
